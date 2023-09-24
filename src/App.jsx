54b6f835e804d6066888be1fd44079fbe6e5ff7d
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-24 12:16:36 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Create from './components/Create';
+import Update from './components/Update';
+import Read from './components/Read';
 
 const USERS_API = 'https://apidata-zkgz.onrender.com/users';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/'  element={<Home />} />
+        <Route path='/create'  element={<Create />} />
+        <Route path='/update/:id'  element={<Update />} />
+        <Route path='/read/:id'  element={<Read />} />
+      </Routes>
+    </div>
   )
 }
 
